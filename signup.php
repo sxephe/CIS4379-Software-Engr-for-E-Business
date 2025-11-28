54373e00c2579b425dedfdad517aac3f4f16ba0c
sxephe <153119929+sxephe@users.noreply.github.com>
2025-11-28 16:26:13 -0600

    Update signup.php
    working on including HTML code into PHP file

--- signup.php
+++ signup.php
@@ -27,27 +27,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Sign Up</title>
-</head>
-<body>
-    <h2>Create an Account</h2>
-    <form method="POST">
-        <label>Name:</label><br>
-        <input type="text" name="name" required><br><br>
-
-        <label>Email:</label><br>
-        <input type="email" name="email" required><br><br>
-
-        <label>Password:</label><br>
-        <input type="password" name="password" required><br><br>
-
-        <button type="submit">Sign Up</button>
-    </form>
-
-    <p>Already have an account? <a href="login.php">Login here</a></p>
-</body>
-</html>
