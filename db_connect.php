0293559316ad0d4e751c6158312bee204ea0583a
AlexTheLion26 <alexrodriguezlhs@gmail.com>
2025-11-28 16:02:04 -0600

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -8,7 +8,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die(" Database connection failed: " . $conn->connect_error);
-} else {
-    echo "Successfully connected to the database: " . htmlspecialchars($dbname);
-}
+} 
 ?>
