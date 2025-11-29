f0fabf29ea96944488f67f7d127e4b2ce4b027c6
sxephe <153119929+sxephe@users.noreply.github.com>
2025-11-28 20:54:15 -0600

    Delete about.php

--- about.php
+++
@@ -1,6 +0,0 @@
-<?php include 'navbar.php'; ?>
-
-<h1>About Campus Meals</h1>
-<p>Campus Meals is a student-run project to make ordering campus food simpler and faster.</p>
-
-<?php include 'footer.php'; ?>
