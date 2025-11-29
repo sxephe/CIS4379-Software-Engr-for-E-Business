e693e07a3b9535399fcde29dd49f51a986de75f4
sxephe <153119929+sxephe@users.noreply.github.com>
2025-11-29 14:17:43 -0600

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php include 'navbar.php'; ?>
-
-<h1>Welcome to Campus Meals!</h1>
-<p>Our goal is to make getting great food on campus quick and easy. Check out our menu or place an order today!</p>
-
-<?php include 'footer.php'; ?>
